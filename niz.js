07fe75cbbc6ba870492a0faba83cfcf2a36c40a2
OgnjenBN <147815524+OgnjenBN@users.noreply.github.com>
2023-10-24 10:30:46 +0200

    Add files via upload

---
+++ niz.js
@@ -0,0 +1,22 @@
+//NIZOVI
+
+const auta = ["volvo", "vw", "mercedes", "audi", "bmw"]
+
+//console.log(auta);
+//console.log("Dužina niza ", niz.length);
+
+const kamioni = new Array("Man", "Scania", "Volvo")
+console.log(kamioni);
+
+console.log(auta.length-1)
+
+console.log(auta[auta.length-1]);
+
+for (let i = 0; i < auta.length; i++) {
+    console.log(auta[i]) }
+
+
+const niz1 = [1,2,3]
+const niz2 = [1,2,3]
+
+console.log(niz1 === niz2);
